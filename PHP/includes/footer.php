fcfbafc1a993e43ba45f43a5379d664bc9229914
Matt Culin <43552534+MatthewCulin@users.noreply.github.com>
2021-01-04 21:10:58 -0500

    Updated After I Realized No PHP

--- PHP/includes/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <p>Links to My Social Media</p>
-    <div>
-        <a class="fa fa-instagram" href="https://www.instagram.com/mattculin/"></a>
-        <a class="fa fa-google" href="https://aboutme.google.com/u/0/?referer=gplus"></a>
-        <a class="fa fa-linkedin" href="https://www.linkedin.com/in/matthew-culin-207950139/"></a>
-    </div>
-    <p>&copy;This page is copy-write protected and cannot be redistributed without the written consent of the owner of this website </p>
-</footer>
