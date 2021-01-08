271e1baa3e7170a3d1ce580d220b99347882160a
Matt Culin <43552534+MatthewCulin@users.noreply.github.com>
2021-01-07 21:43:41 -0500

    Combine JS Files

--- JS/scripts.js
+++ JS/scripts.js
@@ -6,3 +6,19 @@ function responsiveDropdown() {
     x.className = "topnav";
   }
 }
+
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdown menus without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
